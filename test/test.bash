0d6a83f8f0c1bac1f4f682bbb6cf92e1ecb14a58
Hiroaki Ohmatsu <hizzino0817@gmail.com>
2025-01-05 19:12:56 +0900

    change file

--- test/test.bash
+++ test/test.bash
@@ -6,13 +6,15 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 20 ros2 run mypkg battery & sleep 5
+timeout 20 ros2 run mypkg battery & 
+
+sleep 2
 
 
-timeout 10 ros2 topic echo /battery > /tmp/mypkg.log & sleep 5
+timeout 10 ros2 topic echo /battery > /tmp/mypkg.log 
 
 if grep -q "data:" /tmp/mypkg.log; then
     cat /tmp/mypkg.log | grep '91'
 else
-    echo"data: not found"
+    echo "data: not found"
 fi
