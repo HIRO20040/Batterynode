eefacd86beba6b9be47ffefd59e571b3bf78f46b
Hiroaki Ohmatsu <hizzino0817@gmail.com>
2025-01-05 18:20:46 +0900

    file  commit

--- test/test.bash
+++ test/test.bash
@@ -6,7 +6,9 @@ dir=~
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 10 ros2 run mypkg battery > /tmp/mypkg.log
+timeout 10 ros2 run mypkg battery & sleep 5
+
+timeout 10 ros2 topic echo /battery > /tmp/mypkg.log
 
 cat /tmp/mypkg.log |
 grep 'data: 96'
