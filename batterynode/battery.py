acc13f2db0f8030ae7cc46b1765a5155403533ff
Hiroaki Ohmatsu <hizzino0817@gmail.com>
2025-01-10 17:31:59 +0900

    Add files

--- batterynode/battery.py
+++ batterynode/battery.py
@@ -1,28 +1,52 @@
 # SPDX-FileCopyrightText: 2025 Hiroaki Ohmatsu
 # SPDX-License-Identifier: BSD-3-Clause
 
+
+import psutil
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int16
+from std_msgs.msg import String
+
+class BatteryNode(Node):
+
+    def __init__(self):
+        super().__init__('battery')
+        
+        self.publisher_ = self.create_publisher(String, 'battery_info', 10)  
+        
+        self.timer = self.create_timer(2.0, self.publish_battery_info)  
+
+        self.get_logger().set_level(rclpy.logging.LoggingSeverity.WARN)
+        
+        self.declare_parameter('battery_warning_threshold', 20)
+    
+    def publish_battery_info(self):
+        battery = psutil.sensors_battery()
+        if battery:
+            battery_info = f"バッテリー残量: {battery.percent}% | 電源接続中: {'はい' if battery.power_plugged else 'いいえ'}"
+            if not battery.power_plugged:
+                battery_info += f" | 残り時間: {battery.secsleft // 60}分"
+        
+            battery_warning_threshold = self.get_parameter('battery_warning_threshold').value
+            if battery.percent < battery_warning_threshold:
+                battery_info += f" | 警告: バッテリー残量が{battery_warning_threshold}%を下回りました！"
 
 
-rclpy.init()
-node = Node("battery")
-pub = node.create_publisher(Int16, "battery", 10)
-n = 100
 
+        else:
+            battery_info = "バッテリー情報は取得できません。"
+        
+        
+        msg = String()
+        msg.data = battery_info
+        self.publisher_.publish(msg)
+        self.get_logger().info(f"Published: {battery_info}")
 
-
-def cb():
-    global n
-    msg = Int16()
-    msg.data = n
-    pub.publish(msg)
-    n -= 1
 
 def main():
-        node.create_timer(1.0, cb)
-        rclpy.spin(node)
+    rclpy.init()
+    node = BatteryNode()  
+    rclpy.spin(node)  
 
 if __name__ == '__main__':
     main()
--- test/test.bash
+++ test/test.bash
@@ -3,21 +3,29 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 
+
 dir=~
-[ "$1" != "" ] && dir="$1"
+[ "$1" != "" ] && dir="$1"  
+
 
 cd $dir/ros2_ws
 colcon build
 source $dir/.bashrc
-timeout 20 ros2 run batterynode battery & 
+
+
+timeout 20 ros2 run batterynode battery &
+
 
 sleep 2
 
 
-timeout 10 ros2 topic echo /battery > /tmp/batterynode.log 
+timeout 10 ros2 topic echo /battery_info > /tmp/batterynode.log
 
-if grep -q "data:" /tmp/batterynode.log; then
-    cat /tmp/batterynode.log | grep '91'
-else
-    echo "data: not found"
+
+if grep -q "バッテリー残量" /tmp/batterynode.log; then
+    
+    cat /tmp/batterynode.log | grep 'バッテリー残量'
+
+
 fi
+
